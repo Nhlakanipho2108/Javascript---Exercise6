3f496c686b9d08e9efbe5024b22471f95a150cd8
Nhlakanipho.Luthuli <Nhlakanipholuthuli2108@gmail.com>
2026-05-27 11:53:49 +0200

    initial commit

---
+++ functions.js
@@ -0,0 +1,17 @@
+// Create a function called greet that takes a name parameter
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+// Create a function called add that takes two parameters
+function add(a, b) {
+    return a + b;
+}
+
+// Call both functions and store their results in variables
+let greetingMessage = greet("Nhlakanipho Luthuli , Ihtishaam Johnson");
+let total = add(10, 5);
+
+// Print the results to the console
+console.log(greetingMessage);
+console.log(total);
